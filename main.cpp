014676a8883d9e122f2b39daaaf0d19b094bb9ea
Alchemic-Eclipse <alchemic.eclipse@proton.me>
2026-08-12 14:38:59 +0530

    day 1: set up basic body and motion

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
-//
-// Created by Light on 12/08/26.
-//
+#include "iostream"
+
+int main() {
+   struct Body {
+      double m;
+      double x;
+      double y;
+      double vx;
+      double vy;
+   };
+
+   Body earth{};
+   earth.m = 5.972e24;  // kg
+   earth.x = 1.496e11;  // m
+   earth.y = 0;
+   earth.vx = 0;  // m/s
+   earth.vy = -29780;
+
+   double dt = 1.0; // sec
+
+   earth.x += earth.vx * dt;
+   earth.y += earth.vy * dt;
+
+   std::cout << "x = " << earth.x << '\n';
+   std::cout << "y = " << earth.y << '\n';
+
+}
+
+
