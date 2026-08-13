778560d453a3a50cdb871632790514919631afbf
Alchemic-Eclipse <alchemic.eclipse@proton.me>
2026-08-13 17:08:53 +0530

    Gravitas is now 3D

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 
-struct Vector2 {
+struct Vector3 {
       double x;
       double y;
+      double z;
    };
 
 struct Body {
    double m;
-   Vector2 position;
-   Vector2 velocity;
-   Vector2 acceleration;
+   Vector3 position;
+   Vector3 velocity;
+   Vector3 acceleration;
 };
 
-Vector2 add(Vector2 a, Vector2 b) {
-   Vector2 result{};
+Vector3 add(Vector3 a, Vector3 b) {
+   Vector3 result{};
 
    result.x = a.x + b.x;
    result.y = a.y + b.y;
+   result.z = a.z + b.z;
    return result;
 }
 
@@ -27,23 +29,15 @@ int main() {
 
    earth.position.x = 1.496e11;
    earth.position.y = 0;
+   earth.position.z = 0;
 
    earth.velocity.x = 0;
-   earth.velocity.y = -29780;
-
-   earth.acceleration.x = 0.0;
-   earth.acceleration.y = -9.8;
+   earth.velocity.y = 29780;
+   earth.velocity.z = 0;
 
    const double dt = 1.0; // sec
 
-   while (true) {
-      earth.position.x += earth.velocity.x * dt + 0.5 * earth.acceleration.x * dt * dt;
-      earth.position.y += earth.velocity.y * dt + 0.5 * earth.acceleration.y * dt * dt;
 
-      earth.velocity.x += earth.acceleration.x * dt;
-      earth.velocity.y += earth.acceleration.y * dt;
-      std::cout << "Position: " << earth.position.x << "," << earth.position.y << " " << "Velocity: " << earth.velocity.x << "," << earth.velocity.y << "\n";
-   }
 }
 
 
