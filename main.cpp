c5c2fd24528099a53d33e1931dbfe0704aa8dd9b
Alchemic-Eclipse <alchemic.eclipse@proton.me>
2026-08-13 19:50:11 +0530

    Completed Symplectic Euler integrator

--- main.cpp
+++ main.cpp
@@ -40,30 +40,51 @@ Vector3 multiply(Vector3 a, double scalar) {
    return result;
 }
 
-double magnitude(Vector3 a) {
-   double c = sqrt((a.x * a.x) + (a.y * a.y) + (a.z * a.z));
+double magnitude(Vector3 v) {
+   double c = sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
    return c;
+}
+
+Vector3 gravitationalAcceleration(Body a, Body b){
+   Vector3 r = subtract(b.position, a.position);  // Vector from A to B
+   Vector3 direction = multiply(r, 1.0/magnitude(r));
+
+   double acceleration = ((1.0) * (b.m))/(magnitude(r) * magnitude(r)); // G = 1
+   return multiply(direction, acceleration);
 }
 
 int main() {
 
-   Body earth{};
-   earth.m = 5.972e24;
+   // Body earth{};
+   // earth.m = 5.972e24;
+   const double dt = 0.001; // sec
+   //
+   // earth.position.x = 1.496e11;
+   // earth.position.y = 0;
+   // earth.position.z = 0;
+   //
+   // earth.velocity.x = 0;
+   // earth.velocity.y = 29780;
+   // earth.velocity.z = 0;
 
-   earth.position.x = 1.496e11;
-   earth.position.y = 0;
-   earth.position.z = 0;
 
-   earth.velocity.x = 0;
-   earth.velocity.y = 29780;
-   earth.velocity.z = 0;
+   Body a1{1,0,0,0,0,0.2236,0};
+   Body a2{1,10,0,0,0,-0.2236,0};
 
-   const double dt = 1.0; // sec
+   while (true) {
+      // std::cout << acc1.x << "," << acc1.y << "," << acc1.z << "," << "\n";
+      Vector3 acc1 = gravitationalAcceleration(a1, a2);
+      Vector3 acc2 = gravitationalAcceleration(a2, a1);
 
-   // Testing
-   Vector3 a {1.0, 1.0, 1.0};
+      a1.velocity = add(a1.velocity, multiply(acc1, dt));
+      a1.position = add(a1.position, multiply(a1.velocity, dt));
 
-   std::cout << magnitude(a);
+      a2.velocity = add(a2.velocity, multiply(acc2, dt));
+      a2.position = add(a2.position, multiply(a2.velocity, dt));
+
+      std::cout << a1.position.x << ", " << a1.position.y << ", " << a1.position.z << "..... " << a1.velocity.x << ", " << a1.velocity.y << ", " << a1.velocity.z << "\n";
+
+   }
 }
 
 
