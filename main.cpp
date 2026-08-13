e6f88e383222477489bdbad996ed4f60f802fc3a
Alchemic-Eclipse <alchemic.eclipse@proton.me>
2026-08-13 13:11:29 +0530

    Checkpoint before simulating gravity

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
-// #include "iostream"
+#include <iostream>
 
-int main() {
-
-   struct Vector2 {
+struct Vector2 {
       double x;
       double y;
    };
 
-   struct Body {
-      double m;
-      Vector2 position;
-      Vector2 velocity;
-   };
+struct Body {
+   double m;
+   Vector2 position;
+   Vector2 velocity;
+   Vector2 acceleration;
+};
+
+Vector2 add(Vector2 a, Vector2 b) {
+   Vector2 result{};
+
+   result.x = a.x + b.x;
+   result.y = a.y + b.y;
+   return result;
+}
+
+int main() {
 
    Body earth{};
    earth.m = 5.972e24;
@@ -22,11 +31,19 @@ int main() {
    earth.velocity.x = 0;
    earth.velocity.y = -29780;
 
-   double dt = 1.0; // sec
+   earth.acceleration.x = 0.0;
+   earth.acceleration.y = -9.8;
 
-   earth.position.x += earth.velocity.x * dt;
-   earth.position.y += earth.velocity.y * dt;
+   const double dt = 1.0; // sec
 
+   while (true) {
+      earth.position.x += earth.velocity.x * dt + 0.5 * earth.acceleration.x * dt * dt;
+      earth.position.y += earth.velocity.y * dt + 0.5 * earth.acceleration.y * dt * dt;
+
+      earth.velocity.x += earth.acceleration.x * dt;
+      earth.velocity.y += earth.acceleration.y * dt;
+      std::cout << "Position: " << earth.position.x << "," << earth.position.y << " " << "Velocity: " << earth.velocity.x << "," << earth.velocity.y << "\n";
+   }
 }
 
 
